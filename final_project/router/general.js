9559c025fd796be8e6b639fe0038c9d0e8c5f39f
soran-mirzaei <soran.mirzaei.de@gmail.com>
2026-05-09 03:46:53 -0400

    Fixed axios packege

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-//const axios = require('axios');
+const axios = require('axios');
 
 // چک کردن اینکه آیا کاربر از قبل وجود دارد یا خیر
 const doesExist = (username) => {
